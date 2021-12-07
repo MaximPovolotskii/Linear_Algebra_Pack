cbd026d5ea48415f5595e352e232b4c19559831b
EmelianovaTG <emelianova.tg@phystech.edu>
2021-12-07 20:03:26 +0300

    Fix

--- matrix.h
+++ matrix.h
@@ -1,130 +1,368 @@
-#ifndef LINALG_QR_DECOMPOSITION_H
-#define LINALG_QR_DECOMPOSITION_H
+//
+// Created by Максим on 17.11.2021.
+//
 
-#include "matrix.h"
+#ifndef LINALG_MATRIX_H
+#define LINALG_MATRIX_H
 #include <vector>
-#include <complex>
-#include <type_traits>
+#include <exception>
+#include "matrix_multiplication.h"
 
-template<typename T>
-struct is_complex : public std::false_type {};
+const double EPS = 1E-9;
 
-template<typename T>
-struct is_complex<std::complex<T>> : public std::true_type {};
 
-template <typename CT>
-std::enable_if_t<is_complex<CT>::value, Matrix<CT>> ComputeHouseholderFactor(const Vector<CT>& v) {
-    size_t n = v.VertDim();
-    Matrix<CT> res(n, n, NULLMATRIX);
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            res(i, j) = v(i) * std::conj(v(j)) * static_cast<CT>(-2.);
-        }
+class BadMatrixDimension: public std::exception {
+public:
+    const char* what() const throw() override {
+        return "Dimensions do not converge";
     }
-    for (size_t i = 0; i < n; i++)
-        res(i, i) += 1;
-    return res;
+};
+
+class NotSquareMatrix: public BadMatrixDimension {
+public:
+    const char* what() const throw() override {
+        return "Matrix is not square";
+    }
+};
+
+class IndexOutOfMatrix: public std::exception {
+    const char* what() const throw() override {
+        return "Index out of range";
+    }
+};
+
+template<class T>
+const T& min(const T& a, const T& b)
+{
+    return (b < a) ? b : a;
 }
 
-template <typename CT>
-std::enable_if_t<!is_complex<CT>::value, Matrix<CT>> ComputeHouseholderFactor(const Vector<CT>& v) {
-    size_t n = v.VertDim();
-    Matrix<CT> res(n, n, NULLMATRIX);
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            res(i, j) = v(i) * v(j) * (-2.);
+enum Matrix_types
+{
+    NULLMATRIX,  ///нулевая
+    IDENTITY, ///единичная
+    NONE  ///плевать
+};
+
+template <class T>
+class Matrix {
+protected:
+    T* coord = nullptr;
+    size_t vert_dim = 0;
+    size_t horiz_dim = 0;
+    bool transposed = false;
+public:
+    Matrix() = default;
+    Matrix(size_t vert_dim_, size_t horiz_dim_, int matrix_type = NONE): coord(new T[vert_dim_*horiz_dim_]) {
+
+        transposed = false;
+        vert_dim = vert_dim_;
+        horiz_dim = horiz_dim_;
+        if (matrix_type == NULLMATRIX) {
+            for (size_t i = 0; i < vert_dim * horiz_dim; i++) {
+                coord[i] = 0;
+            }
+        }
+        if (matrix_type == IDENTITY) {
+            for (size_t i = 0; i < vert_dim * horiz_dim; i++) {
+                coord[i] = 0;
+            }
+            for (size_t i = 0; i < min(vert_dim, horiz_dim); i++) {
+                coord[i*horiz_dim + i] = 1;
+            }
         }
     }
-    for (size_t i = 0; i < n; i++)
-        res(i, i) += 1;
-    return res;
-}
 
-template<typename CT>
-void ExtractMinor(const Matrix<CT>& mat, size_t d, Matrix<CT> & res) {
+
+    explicit Matrix(T* v, size_t vert_dim_, size_t horiz_dim_): coord(new T[vert_dim_ * horiz_dim_]) {
+        transposed = false;
+        vert_dim = vert_dim_;
+        horiz_dim = horiz_dim_;
+        for (size_t i = 0; i < vert_dim * horiz_dim; i++) {
+            coord[i] = v[i];
+        }
+    }
+
+    Matrix (std::vector<T> v, size_t vert_dim_, size_t horiz_dim_):  coord(new T[vert_dim_ * horiz_dim_]){
+	    transposed = false;
+        vert_dim = vert_dim_;
+        horiz_dim = horiz_dim_;
+        if (v.size() != vert_dim_*horiz_dim_) {
+            throw BadMatrixDimension();
+        }
+        for (size_t i = 0; i < vert_dim * horiz_dim; i++) {
+            coord[i] = v[i];
+        }
+    }
+
+
+    Matrix(Matrix<T> &&rhs) noexcept  {
+        coord = rhs.coord;
+        rhs.coord = nullptr;
+        std::swap(transposed, rhs.transposed);
+        std::swap(vert_dim, rhs.vert_dim);
+        std::swap(horiz_dim, rhs.horiz_dim);
+    }
+
+    Matrix(const Matrix<T> &rhs) {
+        transposed = rhs.transposed;
+        vert_dim = rhs.vert_dim;
+        horiz_dim = rhs.horiz_dim;
+        coord = new T[rhs.vert_dim * rhs.horiz_dim];
+        for (size_t i = 0; i < vert_dim * horiz_dim; i++) {
+            coord[i] = rhs.coord[i];
+        }
+    }
+
+    virtual Matrix &operator=(Matrix<T> &&rhs) noexcept {
+        if (this == &rhs) return *this;
+        Matrix<T> mat(std::move(rhs));
+        std::swap(transposed, mat.transposed);
+        std::swap(coord, mat.coord);
+        std::swap(vert_dim, mat.vert_dim);
+        std::swap(horiz_dim, mat.horiz_dim);
+        return *this;
+    }
+
+    virtual Matrix &operator=(const Matrix<T> &rhs) {
+        if (this == &rhs) return *this;
+        Matrix<T> mat(rhs);
+        std::swap(transposed, mat.transposed);
+        std::swap(coord, mat.coord);
+        std::swap(vert_dim, mat.vert_dim);
+        std::swap(horiz_dim, mat.horiz_dim);
+        return *this;
+    }
+
+    ~Matrix(){
+        if (coord != nullptr)   {delete[] coord;}
+    }
+
+    std::vector<size_t> Shape() const {
+        std::vector<size_t> shape({vert_dim, horiz_dim});
+        return shape;
+    }
+
+    size_t HorizDim() const{
+        return horiz_dim;
+    }
+
+    size_t VertDim() const{
+        return vert_dim;
+    }
+
+    Matrix operator+(const Matrix<T>& rm) const {
+        if (Shape() != rm.Shape())  {
+            throw BadMatrixDimension();
+        }
+        T* res = new T[vert_dim*horiz_dim];
+        for (size_t i = 0; i < vert_dim; ++i) {
+            for (size_t j = 0; j < horiz_dim; ++j) {
+                res[i*horiz_dim + j] = (*this)(i, j) + rm(i, j);
+            }
+        }
+        return Matrix<T>(std::move(res), vert_dim, horiz_dim);
+    }
+
+    Matrix operator-(const Matrix<T>& rm) const {
+        if (Shape() != rm.Shape())  {
+            throw BadMatrixDimension();
+        }
+        T* res;
+        for (size_t i = 0; i < vert_dim; ++i) {
+            for (size_t j = 0; j < horiz_dim; ++j) {
+                res[i*horiz_dim + j] = (*this)(i, j) - rm(i, j);
+            }
+        }
+        return Matrix<T>(std::move(res), vert_dim, horiz_dim);
+    }
+
+    Matrix operator*(const T& val) const {
+        size_t mat_size = vert_dim * horiz_dim;
+        T* res = new T[mat_size];
+        for (size_t i = 0; i < mat_size; i++) {
+            res[i] = coord[i] * val;
+        }
+        return Matrix<T>(std::move(res), vert_dim, horiz_dim);
+    }
+
+    bool operator== (const Matrix<T>& rm) const {
+        if (vert_dim != rm.vert_dim || horiz_dim != rm.horiz_dim) {
+            return false;
+        }
+        for (size_t i = 0; i < vert_dim; ++i) {
+            for (size_t j = 0; j < horiz_dim; j++) {
+                if ((*this)(i, j) != rm(i, j)) { return false; }
+            }
+        }
+        return true;
+    }
+
+    bool operator != (const Matrix<T>& rm) const {
+        return !(this == rm);
+    }
+
+    const T& operator()(size_t i, size_t j) const{
+        if (i < vert_dim && j < horiz_dim) {
+            if (transposed)
+                return coord[j * vert_dim + i];
+            else
+                return coord[i * horiz_dim + j];
+        }
+        else {
+            throw IndexOutOfMatrix();
+        }
+    }
+
+    T& operator()(size_t i, size_t j) {
+        if (i < vert_dim && j < horiz_dim) {
+            if (transposed)
+                return coord[j * vert_dim + i];
+            else
+                return coord[i * horiz_dim + j];
+        }
+        else {
+            throw IndexOutOfMatrix();
+        }
+    }
+
+    Matrix operator* (const Matrix<T>& rm) {
+        if (horiz_dim != rm.vert_dim) {
+            throw BadMatrixDimension();
+        }
+
+        T* res = new T[rm.horiz_dim * vert_dim];
+        T buffer; //T must be 0-compatible type
+
+        for (size_t i = 0; i < vert_dim; i++) {
+            for (size_t j = 0; j < rm.horiz_dim; j++) {
+                buffer = 0;
+                for (size_t k = 0; k < horiz_dim; k++) {
+                    buffer += (*this)(i, k) * rm(k, j);
+                }
+                res[i * rm.horiz_dim + j] = buffer;
+            }
+        }
+        return Matrix(res, vert_dim, rm.horiz_dim);
+    }
+
+    Matrix SmartMult(Matrix<T>& rm) { //to fix non-8-divisible cases and transposed cases
+        if (horiz_dim != rm.vert_dim) {
+            throw BadMatrixDimension();
+        }
+
+        size_t M = (vert_dim/8 + 1) * 8;
+        size_t K = (horiz_dim/8 + 1) * 8;
+        size_t N = (rm.horiz_dim/8 + 1) * 8;
+
+        T* left_mat = (*this).ExpandAndHardTranspose(M, K, false);
+        T* right_mat = rm.ExpandAndHardTranspose(K, N, true);
+
+        T* res = new T[M * N];
+        T* res_norm = new T[vert_dim * rm.horiz_dim];
+        gemm<T> multiply;
+        multiply(M, K, N, left_mat, right_mat, res);
+
+        for (size_t i = 0; i < vert_dim; ++i) {
+            size_t cur_raw = i * rm.horiz_dim;
+            size_t cur_8raw = i * N;
+            for (size_t j = 0; j < rm.horiz_dim; ++j) {
+                res_norm[cur_raw + j] = res[cur_8raw + j];
+            }
+        }
+        return Matrix(res_norm, vert_dim, rm.horiz_dim);
+    }
+
+    T Determinant() const {
+        if (VertDim() != HorizDim()) {
+            throw NotSquareMatrix();
+        }
+        T det_calc = T(1);
+        std::vector<size_t> v;
+        size_t rank = 0;
+        Matrix<T> B = StraightRun(*this, Matrix<T>(horiz_dim, 1), det_calc, v, rank).first;
+        return det_calc;
+    }
+
+    Matrix<T> Inverse() const {
+        if (VertDim() != HorizDim()) {
+            throw NotSquareMatrix();
+        }
+        T det_calc = T(0);
+        size_t rank = 0;
+        std::pair<Matrix<T>, Matrix<T>> p;
+        std::vector<size_t> v;
+
+        p = StraightRun(*this, Matrix<T>(horiz_dim, horiz_dim, IDENTITY), det_calc, v, rank);
+        p = ReverseRun(p.first, p.second, v, rank);
+        return p.second;
+    };
+
+
+    size_t Rank() const {
+        size_t rank = 0;
+        T det_calc = T(0);
+        std::vector<size_t> v;
+        Matrix<T> B = StraightRun(*this, Matrix<T>(vert_dim, 1), det_calc, v, rank).first;
+        return rank;
+    }
+
+    void Transpose() {
+        transposed = !transposed;
+        std::swap(vert_dim, horiz_dim);
+    }
+
+    T* ExpandAndHardTranspose(size_t M, size_t K, bool flip) {
+        T* left_mat;
+        left_mat = new T[M * K];
+        if (!flip) {
+            for (size_t i = 0; i < M; ++i) {
+                size_t cur_raw = i * K;
+                if (i < vert_dim) {
+                    for (size_t j = 0; j < K; ++j) {
+                        if (j < horiz_dim)
+                            left_mat[cur_raw + j] = (*this)(i, j);
+                        else
+                            left_mat[cur_raw + j] = 0;
+                    }
+                } else {
+                    for (size_t j = 0; j < K; ++j) {
+                        left_mat[cur_raw + j] = 0;
+                    }
+                }
+            }
+        }
+        else {
+            for (size_t j = 0; j < K; ++j) {
+                size_t cur_col = j * M;
+                if (j < horiz_dim) {
+                    for (size_t i = 0; i < M; ++i) {
+                        if (i < vert_dim)
+                            left_mat[cur_col + i] = (*this)(i, j);
+                        else
+                            left_mat[cur_col + i] = 0;
+                    }
+                } else {
+                    for (size_t i = 0; i < M; ++i) {
+                        left_mat[cur_col + i] = 0;
+                    }
+                }
+            }
+        }
+        return left_mat;
+    }
+};
+template<typename T>
+void ShowMatrix(const Matrix<T>&  mat)
+{
     for (size_t i = 0; i < mat.VertDim(); i++) {
-        for (size_t j = 0; j < mat.HorizDim(); j++)
-            res(i,j) = 0;
+        for (size_t j = 0; j < mat.HorizDim(); j++) {
+            printf(" %10.3f", mat(i,j));
+        }
+        printf("\n");
     }
-    for (size_t i = 0; i < d; i++)
-            res(i,i) = 1;
-    for (size_t i = d; i < mat.VertDim(); i++) {
-        for (size_t j = d; j < mat.HorizDim(); j++)
-            res(i,j) = mat(i,j);
-    }
+    printf("\n");
 }
 
-template<typename CT>
-void ExtractColumn(const Matrix<CT> &mat, Vector<CT>& v, size_t c){
-    for (size_t i = 0; i < mat.VertDim(); i++)
-        v(i) = mat(i, c);
-}
 
-template <typename CT>
-std::enable_if_t<is_complex<CT>::value, void> QRDecomposition(const Matrix<CT>& mat, Matrix<CT>& Q, Matrix<CT>& R) {
-    size_t vert_dim = mat.VertDim();
-    size_t horiz_dim = mat.HorizDim();
-
-    std::vector<Matrix<CT>> qv(vert_dim);
-    Matrix<CT> z(mat);
-    Matrix<CT> z1(vert_dim, horiz_dim);
-    Vector<CT> e(vert_dim), x(vert_dim);
-    for (size_t k = 0; k < horiz_dim && k < vert_dim - 1; k++) {
-        ExtractMinor(z, k, z1);
-        ExtractColumn(z1, x, k);
-
-        CT a;
-
-        auto r = std::abs(Norm(x));
-        auto phi = std::arg(mat(k, k));
-        a = std::polar(r, phi);
-        a = -a;
-        for (size_t i = 0; i < e.VertDim(); i++)
-            e(i) = (i == k) ? 1 : 0;
-        e = x + e*a;
-        e = Normalize(e);
-        qv[k] = ComputeHouseholderFactor(e);
-        z = (qv[k] * z1);
-    }
-    Q = qv[0];
-
-    for (size_t i = 1; i < horiz_dim && i < vert_dim - 1; i++) {
-        Q = qv[i] * Q;
-    }
-    R = Q*mat;
-    Q.Transpose();
-}
-
-template <typename CT>
-std::enable_if_t<!is_complex<CT>::value, void> QRDecomposition(const Matrix<CT>& mat, Matrix<CT>& Q, Matrix<CT>& R) {
-    size_t vert_dim = mat.VertDim();
-    size_t horiz_dim = mat.HorizDim();
-    std::vector<Matrix<CT>> qv(vert_dim);
-
-    Matrix<CT> z(mat);
-    Matrix<CT> z1(vert_dim, horiz_dim);
-    Vector<CT> e(vert_dim), x(vert_dim);
-    for (size_t k = 0; k < horiz_dim && k < vert_dim - 1; k++) {
-        ExtractMinor(z, k, z1);
-        ExtractColumn(z1, x, k);
-        CT a = Norm(x);
-        if (mat(k,k) > 0) {a = -a;}
-        for (size_t i = 0; i < e.VertDim(); i++)
-            e(i) = (i == k) ? 1 : 0;
-        e = x + e*a;
-        e = Normalize(e);
-        // qv[k] = I - 2 *e*e^T
-        qv[k] = ComputeHouseholderFactor(e);
-        z = (qv[k] * z1);
-
-    }
-    Q = qv[0];
-
-    for (size_t i = 1; i < horiz_dim && i < vert_dim - 1; i++) {
-        Q = qv[i] * Q;
-    }
-    R = Q*mat;
-    Q.Transpose();
-}
-
-#endif //LINALG_QR_DECOMPOSITION_H
+#endif //LINALG_MATRIX_H
